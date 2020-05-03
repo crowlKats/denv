fa9e37bb820e9c17278d4788731be142fe418f2d
Slim Hmidi <slim.hmidi1@gmail.com>
2020-05-03 18:16:30 +0200

    update setting keys to deno env

--- mod.ts
+++ mod.ts
@@ -1,16 +1,16 @@
 export function parse(string: string) {
 	const lines = string.split(/\n|\r|\r\n/).filter(line => line.startsWith("#") ? false : !!line);
-	
+
 	return Object.fromEntries(lines.map(entry => {
 		let [key, val] = entry.split("=");
 		const quoteRegex = /^['"](.*)['"]$/;
-		
+
 		if (quoteRegex.test(val)) {
 			val = val.replace(quoteRegex, "$1");
 		} else {
 			val = val.trim();
 		}
-		
+
 		return [key, val];
 	}));
 }
@@ -19,9 +19,10 @@ export async function load(path: string = ".env") {
 	const file = await Deno.readFile(path);
 	const decoder = new TextDecoder();
 	const dotEnvs = parse(decoder.decode(file));
-	
+
 	for (const [key, val] of Object.entries(dotEnvs)) {
-		Deno.env.set(key, val);
+		const env = Deno.env();
+		env[key] = val;
 	}
 }
 
