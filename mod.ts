fbdbf9a684a93cfd55ed7d0b10c56ff6d2381a3d
crowlkats <crowlkats@gmail.com>
2020-05-20 02:07:23 +0200

    Trim keys

--- mod.ts
+++ mod.ts
@@ -1,17 +1,17 @@
 export function parse(string: string) {
 	const lines = string.split(/\n|\r|\r\n/).filter(line => line.startsWith("#") ? false : !!line);
-	
+
 	return Object.fromEntries(lines.map(entry => {
 		let [key, val] = entry.split("=");
 		const quoteRegex = /^['"](.*)['"]$/;
-		
+
 		if (quoteRegex.test(val)) {
 			val = val.replace(quoteRegex, "$1");
 		} else {
 			val = val.trim();
 		}
-		
-		return [key, val];
+
+		return [key.trim(), val];
 	}));
 }
 
@@ -19,7 +19,7 @@ export async function load(path: string = ".env") {
 	const file = await Deno.readFile(path);
 	const decoder = new TextDecoder();
 	const dotEnvs = parse(decoder.decode(file));
-	
+
 	for (const [key, val] of Object.entries(dotEnvs)) {
 		Deno.env.set(key, val);
 	}
