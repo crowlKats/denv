82131b4fedad98eebdb534683437c52ba48d5168
crowlkats <13135287+crowlKats@users.noreply.github.com>
2020-08-04 00:45:15 +0200

    format

--- mod.ts
+++ mod.ts
@@ -1,29 +1,30 @@
 export function parse(string: string) {
-	const lines = string.split(/\n|\r|\r\n/).filter(line => line.startsWith("#") ? false : !!line);
+  const lines = string.split(/\n|\r|\r\n/).filter((line) =>
+    line.startsWith("#") ? false : !!line
+  );
 
-	return Object.fromEntries(lines.map(entry => {
-		let [key, val] = entry.split("=");
-		const quoteRegex = /^['"](.*)['"]$/;
+  return Object.fromEntries(lines.map((entry) => {
+    let [key, val] = entry.split("=");
+    const quoteRegex = /^['"](.*)['"]$/;
 
-		if (quoteRegex.test(val)) {
-			val = val.replace(quoteRegex, "$1");
-		} else {
-			val = val.trim();
-		}
+    if (quoteRegex.test(val)) {
+      val = val.replace(quoteRegex, "$1");
+    } else {
+      val = val.trim();
+    }
 
-		return [key.trim(), val];
-	}));
+    return [key.trim(), val];
+  }));
 }
 
 export async function load(path: string = ".env") {
-	const file = await Deno.readFile(path);
-	const decoder = new TextDecoder();
-	const dotEnvs = parse(decoder.decode(file));
+  const file = await Deno.readFile(path);
+  const decoder = new TextDecoder();
+  const dotEnvs = parse(decoder.decode(file));
 
-	for (const [key, val] of Object.entries(dotEnvs)) {
-		Deno.env.set(key, val);
-	}
+  for (const [key, val] of Object.entries(dotEnvs)) {
+    Deno.env.set(key, val);
+  }
 }
-
 
 await load();
