23b4946e8d58f166746c70507d068e0885be5ffc
Halvard Mørstad <33620089+halvardssm@users.noreply.github.com>
2021-05-26 13:18:55 +0200

    Reworked load options (#5)

--- README.md
+++ README.md
@@ -1,12 +1,26 @@
 # denv
+
 A module similar to [dotenv](https://github.com/motdotla/dotenv), but for Deno
 
 ## Usage
-You can import the `load` function which will use the `.env` file from the current directory, or you can pass the path to an `env` file:
+
+You can import the `load` function which will use the `.env` file from the
+current directory, or you can pass the path to an `env` file. Here is an example
+with all default values:
+
 ```ts
 import { load } from "https://deno.land/x/denv@2.0.0/mod.ts";
-await load("myenvfile");
+await load({
+  /** The path of the env file */
+  path: ".env",
+  /** If true, won't overwrite existing variables */
+  priorityEnv: false,
+  /** Will not throw an error if file is not found */
+  ignoreMissingFile: false,
+});
 ```
 
 ## Env File Rules
-The rules are the same as dotenv, except `double quoted values expand new lines` is not implemented 
+
+The rules are the same as dotenv, except `double quoted values expand new lines`
+is not implemented
--- tests/test.ts
+++ tests/test.ts
@@ -1,14 +1,37 @@
-import { assertEquals } from "https://deno.land/std@0.63.0/testing/asserts.ts";
+import {
+  assertEquals,
+  assertThrowsAsync,
+} from "https://deno.land/std@0.97.0/testing/asserts.ts";
 import { load } from "../mod.ts";
 
 Deno.test("unquoted", async () => {
-  await load("./tests/.env");
+  await load({ path: "./tests/.env" });
 
   assertEquals(Deno.env.get("FOO"), "bar");
 });
 
 Deno.test("quoted", async () => {
-  await load("./tests/.env");
+  await load({ path: "./tests/.env" });
 
   assertEquals(Deno.env.get("HELLO"), '"world"');
+});
+
+Deno.test("prioritize existing env", async () => {
+  Deno.env.set("FOO", "bear");
+  await load({ path: "./tests/.env", priorityEnv: true });
+
+  assertEquals(Deno.env.get("FOO"), "bear");
+});
+
+Deno.test("throws if file not found", async () => {
+  await assertThrowsAsync(
+    async () => {
+      await load({ path: "./tests/.env2" });
+    },
+    Deno.errors.NotFound,
+  );
+});
+
+Deno.test("does not throw if file not found", async () => {
+  await load({ path: "./tests/.env2", ignoreMissingFile: true });
 });
